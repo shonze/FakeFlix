2996610aa6e3f221a1c5807656a0d89584c734cd
NetanelNakar <netanelnakar07@gmail.com>
2025-01-26 19:58:31 +0200

    a little bit more

--- webapp/src/TopMenu/TopMenu.js
+++ webapp/src/TopMenu/TopMenu.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
 import './TopMenu.css';
 import CategorySearch from '../CategorySearch/CategorySearch'
 import Theme from '../Theme/Theme';
 
 const TopMenu = () => {
     const [isTop, setIsTop] = useState(true);
+    const navigate = useNavigate();
 
     useEffect(() => {
         const handleScroll = () => {
@@ -57,14 +59,21 @@ const TopMenu = () => {
                     <Theme />
                 </ul>
             </div>
-            <div className="d-flex justify-content-center align-items-center">
+            <div className="d-flex justify-content-center align-items-center bg-white rounded shadow">
                 <div className="position-relative">
-                    <input type="text"
-                        className="form-control rounded-pill search-bar pe-5"
-                        placeholder="Search..."
-                        aria-label="Search" />
-                    <button className="btn position-absolute top-50 end-0 translate-middle-y" type="button">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                    <button
+                        className="btn position-absolute top-50 end-0 translate-middle-y bg-white border rounded-circle shadow"
+                        type="button"
+                        onClick={() => navigate(`/category/`)}
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            className="bi bi-search"
+                            viewBox="0 0 16 16"
+                        >
                             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                         </svg>
                     </button>
