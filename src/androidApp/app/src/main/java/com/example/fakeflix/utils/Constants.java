20a96d326833bf593fb2fadbd9a09258f74a07c1
NetanelNakar <netanelnakar07@gmail.com>
2025-02-09 14:20:13 +0200

    edited admin page colors

--- src/androidApp/app/src/main/java/com/example/fakeflix/utils/Constants.java
+++ src/androidApp/app/src/main/java/com/example/fakeflix/utils/Constants.java
@@ -1,5 +1,5 @@
 package com.example.fakeflix.utils;
 
 public class Constants {
-    public static final String BASE_URL = "http://192.168.1.122:8080";
+    public static final String BASE_URL = "http://10.0.2.2:8080";
 }
